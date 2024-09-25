84738efb494caa44f03d8b4aac3ef905a7742144
Yu Tian <yu.tian@aalto.fi>
2024-09-25 19:19:08 +0300

    create gpu env

--- chat-with-pdf/chat_with_pdf.py
+++ chat-with-pdf/chat_with_pdf.py
@@ -14,13 +14,13 @@ data = loader.load()
 text_splitter=RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
 text_chunks=text_splitter.split_documents(data)
 print('num of text chunks', len(text_chunks))
-
-embeddings=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2', model_kwargs={'device':'cpu'})
+print(text_chunks[0])
+embeddings=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2', model_kwargs={'device':'cuda'})
 vector_store=FAISS.from_documents(text_chunks, embeddings)
 
 model_path = os.environ.get('MODEL_WEIGHTS')
 print('Model loading...')
-llm = LlamaCpp(model_path=model_path, verbose=False)
+llm = LlamaCpp(model_path=model_path ,n_gpu_layers=-1, verbose=False ,n_ctx=1024)
 
 template="""Use the following pieces of information to answer the user's question.
 If you dont know the answer just say you know, don't try to make up an answer.
@@ -44,6 +44,6 @@ while True:
         sys.exit()
     if query=='':
         continue
-    result=chain({'query':query})
+    result=chain.invoke({'query':query})
     print(f"Answer:{result['result']}")
 
