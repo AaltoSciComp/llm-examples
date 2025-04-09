a01c0d7750f07382086e8c8a4b114bbb1af10470
Yu Tian <yu.tian@aalto.fi>
2025-04-09 18:44:51 +0300

    update hug example

--- huggingface-models/submit.sh
+++ huggingface-models/submit.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=00:25:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=40GB
-#SBATCH --gpus=1                          # Request 1 GPU
+#SBATCH --gpus=1             # Request 1 GPU
 #SBATCH --partition=gpu-v100-32g
 #SBATCH --output huggingface-gpu.%J.out
 #SBATCH --error huggingface-gpu.%J.err
@@ -10,11 +10,11 @@
 # Set HF_HOME to /scratch/shareddata/dldata/huggingface-hub-cache
 module load model-huggingface/all
 
-# Load scientific Python environment
+# Load Python environment
 module load scicomp-llm-env
 
 # Force transformer to load model(s) from local hub instead of download and load model(s) from remote hub.
 export TRANSFORMERS_OFFLINE=1
 export HF_HUB_OFFLINE=1
 
-python your_script.py
+python -u your_script.py
--- huggingface-models/your_script.py
+++ huggingface-models/your_script.py
@@ -1,26 +1,72 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from transformers import BitsAndBytesConfig
 import torch
 
-model_name = "meta-llama/Llama-3.1-8B"
+model_id = "Qwen/Qwen2.5-14B-Instruct-1M"
 
-# Load tokenizer
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+# ====== LOAD MODEL DIRECTLY ======
+print("="*20)
+print("Loading model directly")
 
-# Load model with bfloat16 on GPU
 model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    torch_dtype=torch.bfloat16,  # Use bfloat16
+    model_id,
+    torch_dtype=torch.bfloat16,
     load_in_8bit=True,
-    device_map="auto"            # Automatically maps to GPU if available
+    device_map="auto"
 )
+tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-# Prepare input
 prompt = "How many stars in the space?"
-model_inputs = tokenizer([prompt], return_tensors="pt").to("cuda")  # Send inputs to GPU
-input_length = model_inputs.input_ids.shape[1]
+messages = [
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": prompt}
+]
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True
+)
+model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
 
-# Generate response
-generated_ids = model.generate(**model_inputs, max_new_tokens=20)
+generated_ids = model.generate(
+    **model_inputs,
+    max_new_tokens=512
+)
+generated_ids = [
+    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+]
 
-# Decode and print result
-print(tokenizer.batch_decode(generated_ids[:, input_length:], skip_special_tokens=True)[0])
+response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+print("Response:", response)
+
+print("="*20)
+# ====== USE A PIPELINE AS A HIGH-LEVEL HELPER ======
+print("Using pipeline")
+
+quantization_config = BitsAndBytesConfig(
+    load_in_quant4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16,
+)
+model_kwargs = {
+    "torch_dtype": torch.bfloat16,
+    "quantization_config": quantization_config,
+}
+
+pipe = pipeline(
+    "text-generation",
+    model=model_id,
+    device_map="auto",
+    max_new_tokens=512, # We are expecting a single word as output
+    model_kwargs=model_kwargs,
+    temperature=0.1, # Small strictly positive value, no randomness
+    )
+
+messages = [
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Who are you?"},
+]
+
+pipe_response = pipe(messages)
+print("Response:", pipe_response)
