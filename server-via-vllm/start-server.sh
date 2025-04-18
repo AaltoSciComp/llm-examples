7019dce9cf3e5148aaaa61dfadabb9f0da143c29
Yu Tian <63806966+YuTian8328@users.noreply.github.com>
2025-04-18 13:09:38 +0300

    Update start-server.sh

--- server-via-vllm/start-server.sh
+++ server-via-vllm/start-server.sh
@@ -2,9 +2,8 @@
 
 module load model-huggingface/all
 
-module load mamba
+module load scicomp-llm-env
 
-source activate vllm-env
 export TRANSFORMERS_OFFLINE=1
 
 python -m vllm.entrypoints.openai.api_server --model "$1"
